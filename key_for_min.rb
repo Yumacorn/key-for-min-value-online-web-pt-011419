0daea15cb962012ecfbd54cdf7d40874e298f4f5
Ryan Mui <reachryanmui@gmail.com>
2019-02-08 03:41:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest = {}
   name_hash.each do |name, value|
-    if name_hash[name] < lowest[lowkey]
+    if value < lowest
       lowest[lowkey] = value
     end
   end
